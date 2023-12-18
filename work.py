3ec68ffd972f5fd5602f50e80c1bd05c71b84a3d
iamsnjy <127886889+iamsnjy@users.noreply.github.com>
2023-12-18 13:44:13 +0100

    Update work.py

--- work.py
+++ work.py
@@ -1,5 +1,15 @@
+def permutation_string(s):
+    if len(s)==1:
+        return s
 
-print("Hello World")
+    p=[]
+    for i in range(len(s)):
+        fixed_char=s[i]
+        remaining_char=s[:i]+s[i+1:]
+        for perm in permutation_string(remaining_char):
+            p.append(fixed_char+remaining_char)
+    return p
+print(permutation_string('ABC'))
 
 
 
